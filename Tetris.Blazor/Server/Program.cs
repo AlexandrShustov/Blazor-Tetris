df4751aa63d0543ca19346954a282719bd59d75f
PLARIUM\o.shustov <o.shustov@plarium.com>
2023-01-13 19:50:37 +0200

    Add a home page

--- Tetris.Blazor/Server/Program.cs
+++ Tetris.Blazor/Server/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.WebHost.UseStaticWebAssets();
 
 var app = builder.Build();
 
